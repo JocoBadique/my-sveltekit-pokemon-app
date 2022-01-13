aa8975b678e86f4e8e5ecaa7127d87c92ac4fdaf
unknown <badiquejoco@gmail.com>
2022-01-13 15:53:00 +0800

    update pokedex application

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>About | Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About Svelte Kit Pokedex</h1>
+<p>This is a really cool SvelteKit application that uses Pokemon API!</p>
+
